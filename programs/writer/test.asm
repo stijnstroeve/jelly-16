d4b6b8dad8fc11475da21bd7b249146d1ccd5415
Stijn Stroeve <stijnstroeve@hotmail.com>
2026-08-28 16:39:24 +0200

    Start on writer program

---
+++ programs/writer/test.asm
@@ -0,0 +1,190 @@
+
+.data
+
+; .word 'H', 'A', 'L', 'L', 'O'
+
+; A
+.word 0,1,1,0
+.word 1,0,0,1
+.word 1,0,0,1
+.word 1,1,1,1
+.word 1,0,0,1
+
+; B
+.word 1,1,1,0
+.word 1,0,0,1
+.word 1,1,1,0
+.word 1,0,0,1
+.word 1,1,1,0
+
+; C
+.word 1,1,1,1
+.word 1,0,0,0
+.word 1,0,0,0
+.word 1,0,0,0
+.word 1,1,1,1
+
+; D
+.word 1,1,1,0
+.word 1,0,0,1
+.word 1,0,0,1
+.word 1,0,0,1
+.word 1,1,1,0
+
+; E
+.word 1,1,1,1
+.word 1,0,0,0
+.word 1,1,1,1
+.word 1,0,0,0
+.word 1,1,1,1
+
+; F
+.word 1,1,1,1
+.word 1,0,0,0
+.word 1,1,1,0
+.word 1,0,0,0
+.word 1,0,0,0
+
+; G
+.word 0,1,1,1
+.word 1,0,0,0
+.word 1,0,1,1
+.word 1,0,0,1
+.word 0,1,1,1
+
+; H
+.word 1,0,0,1
+.word 1,0,0,1
+.word 1,1,1,1
+.word 1,0,0,1
+.word 1,0,0,1
+
+; I
+.word 1
+.word 1
+.word 1
+.word 1
+.word 1
+
+; J
+.word 0,0,0,1
+.word 0,0,0,1
+.word 0,0,0,1
+.word 1,0,0,1
+.word 0,1,1,0
+
+; K
+.word 1,0,0,1
+.word 1,0,1,0
+.word 1,1,0,0
+.word 1,0,1,0
+.word 1,0,0,1
+
+; L
+.word 1,0,0,0
+.word 1,0,0,0
+.word 1,0,0,0
+.word 1,0,0,0
+.word 1,1,1,1
+
+; M
+.word 1,0,0,1
+.word 1,1,1,1
+.word 1,0,0,1
+.word 1,0,0,1
+.word 1,0,0,1
+
+
+; N
+.word 1,0,0,1
+.word 1,1,0,1
+.word 1,0,1,1
+.word 1,0,0,1
+.word 1,0,0,1
+
+; O
+.word 0,1,1,0
+.word 1,0,0,1
+.word 1,0,0,1
+.word 1,0,0,1
+.word 0,1,1,0
+
+; P
+.word 0,1,1,0
+.word 1,0,0,1
+.word 1,1,1,0
+.word 1,0,0,0
+.word 0,0,0,0
+
+; Q
+.word 0,1,1,0
+.word 1,0,0,1
+.word 1,0,0,1
+.word 1,0,1,1
+.word 0,1,1,0
+
+; R
+.word 0,1,1,0
+.word 1,0,0,1
+.word 1,1,1,0
+.word 1,0,1,0
+.word 1,0,0,1
+
+; S
+.word 0,1,1,1
+.word 1,0,0,0
+.word 0,1,1,1
+.word 0,0,0,1
+.word 1,1,1,0
+
+; T
+.word 1,1,1
+.word 0,1,0
+.word 0,1,0
+.word 0,1,0
+.word 0,1,0
+
+; U
+.word 1,0,0,1
+.word 1,0,0,1
+.word 1,0,0,1
+.word 1,0,0,1
+.word 0,1,1,0
+
+; V
+.word 1,0,1
+.word 1,0,1
+.word 1,0,1
+.word 1,0,1
+.word 0,1,0
+
+; W
+.word 1,0,1,0,1
+.word 1,0,1,0,1
+.word 1,0,1,0,1
+.word 1,0,1,0,1
+.word 0,1,0,1,0
+
+; X
+.word 1,0,0,1
+.word 0,1,1,0
+.word 0,1,1,0
+.word 1,0,0,1
+.word 1,0,0,1
+
+; Y
+.word 1,0,1
+.word 1,0,1
+.word 0,1,0
+.word 0,1,0
+.word 0,1,0
+
+; Z
+.word 1,1,1,1
+.word 0,0,0,1
+.word 0,0,1,0
+.word 0,1,0,0
+.word 1,1,1,1
+
+; Charachter widths
+.word 4,5
